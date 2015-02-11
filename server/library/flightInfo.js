fcddcbf9b73a286b504aa9a2e25f14ce6fb4b767
Yohannes <personalprojectyohannes@gmail.com>
2015-02-10 23:06:50 -0600

    [FEATURE] add a class that shows the cheapest price of a particular flight

---
+++ server/library/flightInfo.js
@@ -0,0 +1,45 @@
+var request = require('request');
+var apiKeys = require("./config.js");
+
+var FlightInfo = function(){}
+
+FlightInfo.prototype.cheapestPrice = function(origin, destination, departureDate, returningDate, callback){
+
+var requestBody = JSON.stringify({
+						  "request": 
+						  {
+						    "passengers": 
+						    {
+						      "adultCount": 1
+						    },
+						    "slice": 
+						    [
+						      {
+						        "origin": origin,
+						        "destination": destination,
+						        "date": departureDate[2] + "-" + departureDate[0] + "-" +departureDate[1] // YYYY-MM-DD
+						      },
+						      {
+						        "origin": destination,
+						        "destination": origin,
+						        "date": returningDate[2] + "-" + returningDate[0] + "-" +returningDate[1] // YYYY-MM-DD
+						      }
+						    ]
+						  }
+						});
+
+request.post({ 
+			//Google QPX-Express API
+			"url": "https://www.googleapis.com/qpxExpress/v1/trips/search?key=" + apiKeys.googleapisKey,
+				"headers": {
+					    'Content-Type': 'application/json'
+					},
+				"body" : requestBody
+
+			}, function(error, res, data){
+				data = JSON.parse(data);
+				callback(null, data.trips.tripOption[0].saleTotal);
+			});
+}
+
+module.exports = FlightInfo;
