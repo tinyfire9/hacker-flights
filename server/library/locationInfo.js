1347547ef7731285607f9aea68b6d5e97204445f
Yohannes <personalprojectyohannes@gmail.com>
2015-02-10 23:00:53 -0600

    [FEATURE] add a class that returns the latitudinal and longitudinal position of a given city

---
+++ server/library/locationInfo.js
@@ -0,0 +1,17 @@
+var LocationInfo = function(){}
+
+LocationInfo.prototype.toLongtudeAndLatitude = function(city, state, callback){
+	var location;
+	request.get({
+		//Google Geocoding API
+		"url" : "https://maps.googleapis.com/maps/api/geocode/json?address=" + city + ",+" + state + "&key=" + apiKeys.googleapisKey,
+	}, function(error, res, data){
+		data = JSON.parse(data);
+		location = {"latitude" : data.results[0].geometry.location.lat, 
+					"longtude" : data.results[0].geometry.location.lng
+					}
+		callback(null, location);
+	});
+}
+
+module.exports = LocationInfo;
