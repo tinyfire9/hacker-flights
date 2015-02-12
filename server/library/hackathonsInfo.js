49de801dadbf91212b2134250f95e99ed9330ad0
Yohannes <personalprojectyohannes@gmail.com>
2015-02-11 23:58:36 -0600

    [TASK] rename class

--- server/library/hackathonsInfo.js
+++ server/library/hackathonsInfo.js
@@ -1,11 +1,11 @@
-var NearestAirport = require('./nearestAirport.js');
+var Airport = require('./airport.js');
 var FlightInfo = require('./flightInfo.js');
 var fs = require('fs');
 var hackathonsInfo = [];
 var hackathons = require('../model/data.js')
 
 var flightInfo = new FlightInfo();
-var nearestAirport = new NearestAirport();
+var Airport = new Airport();
 
 var HackathonsInfo = function(){}
 
@@ -18,7 +18,7 @@ HackathonsInfo.prototype.findInfo = function(origin, callback){
 	{
 		(function(hackathon)
 			{
-				nearestAirport.findOne(hackathons[hackathon].location.city, hackathons[hackathon].location.state, function(error, nearestAirportInfo){
+				Airport.findNearestAirport(hackathons[hackathon].location.city, hackathons[hackathon].location.state, function(error, nearestAirportInfo){
 					if(error)
 					{
 						throw Error(error);
@@ -26,21 +26,24 @@ HackathonsInfo.prototype.findInfo = function(origin, callback){
 					HackathonLength = hackathons[hackathon].dates.length;
 					hackathonBeginningDate = hackathons[hackathon].dates[0];
 					hackathonEndingDate = hackathons[hackathon].dates[HackathonLength - 1];
-
 					flightInfo.cheapestPrice(origin, nearestAirportInfo.code, hackathonBeginningDate, hackathonEndingDate, function(error, price){
 						if(error)
 						{
 							throw Error(error);
 						}
+
 						hackathonsInfo.push({
 							"hackathonName" : hackathons[hackathon].hackathonName,
 							"dates" : hackathons[hackathon].dates,
 							"location" : hackathons[hackathon].location,
+							"originalLocationCode" : origin,
 							"nearestAirport" : nearestAirportInfo.name,
 							"airportLocation" : nearestAirportInfo.city + ", " + nearestAirportInfo.country,
+							"airportCode" :  nearestAirportInfo.code,
 							"startingPrice" : price
 						});
-						
+						//console.log(hackathonsInfo + "---");
+						//console.log(hackathon + "<-index--arrayLeng->" + hackathons.length);
 						if(hackathon == hackathons.length - 1)
 						{
 							callback(null, hackathonsInfo);
