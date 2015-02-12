3ac55faaf8ec09449753cb95ec84c2529f5db0de
Yohannes <personalprojectyohannes@gmail.com>
2015-02-11 23:50:54 -0600

    [FEATURE] add configuration on routes

--- client/js/app.js
+++ client/js/app.js
@@ -1,2 +1,13 @@
-var app = angular.module("myApp", ["ngResource"]);
+var app = angular.module("myApp", ["ngRoute", "ngResource"]);
 
+app.config(['$routeProvider',  function($routeProvider){
+	
+	$routeProvider
+		.when('/', 
+			{templateUrl : "views/location.html"})
+		.when('/hackathons/:location', 
+			{
+				templateUrl : "views/hackathons.html",
+				controller : "hackathonsListController"
+		});
+}]);
