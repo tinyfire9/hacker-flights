9dca2f9086d8ee99833aab49b47591701eb687fb
Yohannes <personalprojectyohannes@gmail.com>
2015-02-13 20:10:19 -0600

    [TASK] deploy app on heroku

---
+++ server/model/pendingData.js
@@ -0,0 +1,27 @@
+/*
+		{	
+			"hackathonName" : "HackIllinois",
+			"dates" : [[2, 26, 2015], [2, 27, 2015]], //<----------------------NOT FUNCTIONAL
+			"location" : {
+				"city" : "Urbana",
+				"state" : "IL"
+			}
+		},*/
+
+/*
+		{	
+			"hackathonName" : "Citrus Hack",
+			"dates" : [[4, 17, 2015], [4, 18, 2015], [4, 19, 2015]], //Returns Air Reserve Base
+			"location" : {
+				"city" : "Riverside",
+				"state" : "CA"
+			}
+		},
+		{	
+			"hackathonName" : "HackYSU",
+			"dates" : [[4, 17, 2015], [4, 18, 2015], [4, 19, 2015]], //Does not have any flights
+			"location" : {
+				"city" : "Youngstown",
+				"state" : "OH"
+			}
+		}*/
