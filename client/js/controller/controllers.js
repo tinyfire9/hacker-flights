43d95690b48ac5f9faf7c22794ee6ed32c038c5c
Yohannes <personalprojectyohannes@gmail.com>
2015-02-13 18:52:23 -0600

    [TASK] add helper function that concatinates '0' on single digit numbers

--- client/js/controller/controllers.js
+++ client/js/controller/controllers.js
@@ -11,10 +11,36 @@ app.controller("redirect" , ['$scope', '$location',
 
 app.controller("hackathonsList" , ['$scope', '$resource','$routeParams',
 	function($scope, $resource,$routeParams)
-	{
+	{	
 		var hacks = new $resource('/hackathons/' + $routeParams.airportLocation);
 		hacks.query(function(data){
+			data = pad(data);
 			$scope.hackathons = data;
 		});
 	}
 ]);
+
+
+
+
+// helper function that concatinates '0' on single digit numbers 
+
+var pad = function(data){
+	var count = 0;
+	while(count < data.length)
+	{
+		for(var i = 0; i < data[count].dates.length; i++)
+		{
+			if(data[count].dates[i][0] < 10)
+			{
+				data[count].dates[i][0] = "0" + data[count].dates[i][0];
+			}
+			if(data[count].dates[i][1] < 10)
+			{
+				data[count].dates[i][1] = "0" + data[count].dates[i][1];
+			}
+		}
+		count++;
+	}
+	return(data);
+}
