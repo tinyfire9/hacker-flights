8f651ccc7b42536c5bf9a14f406502065bf0cadb
slimfire <personalprojectyohannes@gmail.com>
2015-07-26 21:07:47 -0700

    [FEATURE] check number of api calls every 24 hrs and email me if it is more than 300

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
-var express = require('express');
+var app = require('express')();
 var bodyParser = require('body-parser');
 var serverStatic = require('serve-static');
 var methodOverride = require('method-override');
 var errorHandler = require('errorhandler');
+var routes = require('./controllers/routes.js');
+var hackerFlights = require('./controllers/api/hackerFlights.js');
 var fs = require('fs');
-var routes = require('./server/controllers/routes.js');
-var HackathonsInfo = require('./server/library/hackathonsInfo.js');
-var app = express();
-var Hackathons = new HackathonsInfo();
 
 app.use(serverStatic(__dirname + '/client'));
 app.use(bodyParser.json());
@@ -32,6 +30,9 @@ app.get('*', function(req, res){
 	res.sendFile(__dirname + '/client/views/index.html');
 });
 
+setInterval(function(){
+	hackerFlights.tryNotifyingAPIUsage();
+}, 1500)
 
 app.listen(process.env.PORT || 3000, function(){
 	console.log("Server running ...")
