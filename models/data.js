80c97a141fe17ac129d5f866e7380633a0b66a8f
slimfire <personalprojectyohannes@gmail.com>
2015-07-26 21:24:03 -0700

    [FEATURE] add new hackathons

---
+++ models/data.js
@@ -0,0 +1,181 @@
+var data = [
+		{	
+			"hackathonName" : "PennApps",
+			"dates" : [[9, 4, 2015], [9, 5, 2015], [9, 6, 2015]],
+			"location" : {
+				"city" : "philadelphia",
+				"state" : "PA"
+			}
+		},
+		{	
+			"hackathonName" : "HopHacks",
+			"dates" : [[9, 11, 2015], [9, 12, 2015], [9, 13, 2015]],
+			"location" : {
+				"city" : "Baltimore",
+				"state" : "MD"
+			}
+		},
+		{	
+			"hackathonName" : "MHacks",
+			"dates" : [[9, 11, 2015], [9, 12, 2015], [9, 13, 2015]],
+			"location" : {
+				"city" : "Ann Arbor",
+				"state" : "MI"
+			}
+		},
+		{	
+			"hackathonName" : "Bigred//Hacks",
+			"dates" : [[9, 18, 2015], [9, 19, 2015], [9, 20, 2015]],
+			"location" : {
+				"city" : "Ithaca",
+				"state" : "NY"
+			}
+		},
+		{	
+			"hackathonName" : "WUHack",
+			"dates" : [[9, 18, 2015], [9, 19, 2015], [9, 20, 2015]],
+			"location" : {
+				"city" : "St. Louis",
+				"state" : "MO"
+			}
+		},
+
+		{	
+			"hackathonName" : "HackTX",
+			"dates" : [[9, 26, 2015], [9, 27, 2015]],
+			"location" : {
+				"city" : "Austin",
+				"state" : "TX"
+			}
+		},
+		{	
+			"hackathonName" : "Huskie Hack",
+			"dates" : [[9, 26, 2015], [9, 27, 2015]],
+			"location" : {
+				"city" : "DeKalb",
+				"state" : "IL"
+			}
+		},
+		{	
+			"hackathonName" : "MavHacks",
+			"dates" : [[10, 2, 2015], [10, 3, 2015]],
+			"location" : {
+				"city" : "Arlington",
+				"state" : "TX"
+			}
+		},
+		{	
+			"hackathonName" : "SDHacks",
+			"dates" : [[10, 2, 2015], [10, 3, 2015], [10, 4, 2015]],
+			"location" : {
+				"city" : "San Diego",
+				"state" : "CA"
+			}
+		},
+		{	
+			"hackathonName" : "VandyHacks",
+			"dates" : [[10, 2, 2015], [10, 3, 2015], [10, 4, 2015]],
+			"location" : {
+				"city" : "Nashville",
+				"state" : "TN"
+			}
+		},
+		{	
+			"hackathonName" : "HackNC",
+			"dates" : [[10, 9, 2015], [10, 10, 2015], [10, 11, 2015]],
+			"location" : {
+				"city" : "Chapel Hill",
+				"state" : "NC"
+			}
+		},
+		{	
+			"hackathonName" : "Kent Hack Enough",
+			"dates" : [[10, 9, 2015], [10, 10, 2015], [10, 11, 2015]],
+			"location" : {
+				"city" : "Kent",
+				"state" : "OH"
+			}
+		},
+		{	
+			"hackathonName" : "BoilerMake",
+			"dates" : [[10, 16, 2015], [10, 17, 2015], [10, 18, 2015]],
+			"location" : {
+				"city" : "West Lafayette",
+				"state" : "IN"
+			}
+		},
+		{	
+			"hackathonName" : "HackUMass",
+			"dates" : [[10, 23, 2015], [10, 24, 2015], [10, 25, 2015]],
+			"location" : {
+				"city" : "Amherst",
+				"state" : "MA"
+			}
+		},
+		{	
+			"hackathonName" : "BostonHacks",
+			"dates" : [[10, 31, 2015], [11, 1, 2015]],
+			"location" : {
+				"city" : "Boston",
+				"state" : "MA"
+			}
+		},
+		{	
+			"hackathonName" : "HackUstate",
+			"dates" : [[11, 6, 2015], [11, 7, 2015]],
+			"location" : {
+				"city" : "Logan",
+				"state" : "UT"
+			}
+		},
+		{	
+			"hackathonName" : "HackDuke",
+			"dates" : [[11, 7, 2015], [11, 8, 2015]],
+			"location" : {
+				"city" : "Durham",
+				"state" : "NC"
+			}
+		},
+		{	
+			"hackathonName" : "HackNJIT",
+			"dates" : [[11, 7, 2015], [11, 8, 2015]],
+			"location" : {
+				"city" : "Newark",
+				"state" : "NJ"
+			}
+		},
+		{	
+			"hackathonName" : "HackHarvard",
+			"dates" : [[11, 13, 2015], [11, 14, 2015], [11, 15, 2015]],
+			"location" : {
+				"city" : "Cambridge",
+				"state" : "MA"
+			}
+		},
+		{	
+			"hackathonName" : "CodeRED",
+			"dates" : [[11, 21, 2015], [11, 22, 2015]],
+			"location" : {
+				"city" : "Houston",
+				"state" : "TX"
+			}
+		},
+		{	
+			"hackathonName" : "WildHacks",
+			"dates" : [[11, 21, 2015], [11, 22, 2015]],
+			"location" : {
+				"city" : "Evanston",
+				"state" : "IL"
+			}
+		},
+		{	
+			"hackathonName" : "HackWestern",
+			"dates" : [[11, 27, 2015], [11, 28, 2015], [11, 29, 2015]],
+			"location" : {
+				"city" : "London",
+				"state" : "ON"
+			}
+		}
+	]
+	
+module.exports = data;
