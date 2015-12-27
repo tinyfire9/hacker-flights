46461fa2b3e17c0027eac6863b95681fbf5f74ec
Yohannes <personalprojectyohannes@gmail.com>
2015-12-26 20:14:07 -0600

    [FEATUTRE] add getAirports on utilities

--- controllers/utils/utils.js
+++ controllers/utils/utils.js
@@ -1,9 +1,25 @@
 var request = require('request'),
-		nodemailer = require('nodemailer');
-		config = require('../../config.js');
+	nodemailer = require('nodemailer'),
+	config = require('../../config.js'),
+	xlsx = require('node-xlsx');
 
 var Utils = function(){}
 
+Utils.prototype.getAirports = function(path){
+	var data = xlsx.parse(path);
+	var airports = [];
+	data[0].data.forEach(function(row, i) {
+		if(i > 6 && i < 501)
+		{
+			var airport = {};
+			airport.code = row[2];//airport code
+			airport.city = row[3];//city
+			airport.state = row[0];//state
+			airports.push(airport);
+		}
+	});
+	return airports;
+}
 Utils.prototype.jsonpToJson = function(string){
 	//general format of the parameter : "callback({"processingDurationMillis":10,"authorisedAPI":true,"success":true,"airline":null,"errorMessage":null,"airports":[{"code":"ONT","name":"Ontario Intl","city":"Ontario","country":"United States","lat":34.056,"lng":-117.601194,"terminal":null,"gate":null,"timezone":"America/Los_Angeles"},null]})"
 	//removes "callback(" at the beginning of the string and ")" at the end of the string
