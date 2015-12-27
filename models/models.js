be0594fe7eb7551b1b9ff79d391619d077fa62c5
Yohannes <personalprojectyohannes@gmail.com>
2015-12-26 20:18:57 -0600

    [FEATURE] connect to remote db host and export airport and queries model as one object

---
+++ models/models.js
@@ -0,0 +1,8 @@
+var mongoose = require('mongoose'),
+connection = 'mongodb://db_user:db_password@ds047732.mongolab.com:47732/heroku_46tnnbb3';
+mongoose.connect(connection);
+
+module.exports = {
+	queries : require('./queries.js'),
+	airports : require('./airports.js')
+};
