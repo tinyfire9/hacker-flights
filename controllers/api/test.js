141d7fa72b667404d2da89a4ff4d9dc90146e274
Yohannes <personalprojectyohannes@gmail.com>
2015-12-28 20:58:26 -0600

    [TEST] check methods

---
+++ controllers/api/test.js
@@ -0,0 +1,10 @@
+// var hackerFlights = require('./hackerFlights.js');
+// console.log("heeyy");
+// hackerFlights.updateLocations(function() {
+// 	console.log("status : DONE");
+// })
+
+var api = require('./publicAPI.js')
+// api.getHackathons(function(hackathons){
+// 	console.log('---------------------> Size : ', hackathons.length)
+// });
