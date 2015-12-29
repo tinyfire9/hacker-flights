2248a07706da300caa7c15e620599c8c9993f91e
Yohannes <personalprojectyohannes@gmail.com>
2015-12-28 20:58:41 -0600

    [TEST] check methods

---
+++ controllers/utils/test.js
@@ -0,0 +1,12 @@
+var util = require('./utils.js');
+
+// util.getLongtudeAndLatitude('bemidji', 'minnesota', function(location){
+// 	console.log(location);
+// });
+// console.log(util.getAirports(__dirname + '/vale_eligible_airports.xls'));
+
+// util.getHackathons('http://www.hackalist.org/api/1.0/2016/01.json', function(hackathons) {
+// 	console.log("HEeyyyy : ", hackathons);
+// })
+
+// console.log('HEeyyyy : ', util.getUrls());
