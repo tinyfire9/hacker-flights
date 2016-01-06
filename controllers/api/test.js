c8549c46794355d427de5dfebecea752bd0408dd
Yohannes <personalprojectyohannes@gmail.com>
2016-01-06 00:09:55 -0600

    [TEST] add test on getCheapestPrice

--- controllers/api/test.js
+++ controllers/api/test.js
@@ -8,6 +8,13 @@ var api = require('./publicAPI.js')
 // api.getHackathons(function(hackathons){
 // 	console.log('---------------------> Size : ', hackathons.length)
 // });
-api.findNearestAirport('cass lake', 'Mn', function(error, airport){
-	console.log(error || airport);
+// api.findNearestAirport('cass lake', 'Mn', function(error, airport){
+// 	console.log(error || airport);
+// })
+
+// MSN YNG 2016-11-5 2016-11-7
+api.getCheapestPrice('msn','tol','2016-11-26','2016-11-28', function(err, res) {
+	// body...
+
+	console.log(err, res)
 })
