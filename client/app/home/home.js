8b30cca6cba9c761a1ec37dcae1254fb62283397
slimfire <personalprojectyohannes@gmail.com>
2019-06-08 12:15:38 -0500

    [FEATURE] add places autocomplete feature on departure location input

--- client/app/home/home.js
+++ client/app/home/home.js
@@ -1,3 +1,55 @@
+class RowOptions {
+	rowOptionOnClick = (place) => {
+		return () => {
+			const dropdownElement = document.getElementsByClassName('autocomplete')[0];
+			const searchButtom = document.getElementById('search-button');
+			
+			document.getElementById('input-box').value = place;
+			searchButtom.disabled = false;
+			dropdownElement.hidden = true;
+			this.removeRowOptions(dropdownElement);
+		}
+	};
+	
+	createRowOption = (place) => {
+		const rowOption = document.createElement('DIV');
+	
+		rowOption.onclick = this.rowOptionOnClick(place);
+		rowOption.innerText = place;
+		rowOption.onmouseover = () => {
+			rowOption.style.backgroundColor = '#9dfcc9';
+		}
+		rowOption.onmouseleave = () => {
+			rowOption.style.backgroundColor = '#fff';
+		}
+	
+		return rowOption;
+	};
+
+	removeRowOptions = (dropdownElement) => {
+		while(dropdownElement.firstChild) {
+			dropdownElement.removeChild(dropdownElement.firstChild);
+		}
+	}
+
+	populateAutocompletePlaces = (places) => {
+		const dropdownElement = document.getElementsByClassName('autocomplete')[0];
+		const searchButtom = document.getElementById('search-button');
+		searchButtom.disabled = true;
+		this.removeRowOptions(dropdownElement);
+
+		if( places.length === 0 ) {
+			dropdownElement.hidden = true;
+
+		} else {
+			dropdownElement.hidden = false;
+			places.forEach((place) => {
+				dropdownElement.append(this.createRowOption(place));
+			});
+		}
+	}
+}
+
 angular.module('hackerFlights.home', [])
 	.config(function($stateProvider){
 		$stateProvider.state('home', {
@@ -7,7 +59,9 @@ angular.module('hackerFlights.home', [])
 		});
 	})
 	.controller('homeController', function($scope, $location){
+		const { populateAutocompletePlaces } = new RowOptions();
 		socket.on('hackerFlights.placesAutoComplete', ({ places }) => {
+			populateAutocompletePlaces(places)
 		});
 
 		$scope.listAutocompletePlaces = function() {
